8a111962be1d09e059e2fd5be8dd6d55b22cd906
muhdamean <muhdamean@gmail.com>
2022-06-13 07:50:47 +0100

    code rub

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -22,12 +22,14 @@ export default function ProductDetails(){
     return(
         <Grid container spacing={6}>
             <Grid item xs={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{width:'100%'}}></img>
+                <img src={product.pictureUrl} alt={product.name} style={{width:'100%'}}/>
             </Grid>
             <Grid item xs={6}>
                 <Typography variant='h3'>{product.name}</Typography>
                 <Divider sx={{mb:2}}></Divider>
-                <Typography variant='h4' color='secondary'>${(product.price /100).toFixed(2)}</Typography>
+                <Typography variant='h4' color='secondary'>
+                    ${(product.price /100).toFixed(2)}
+                </Typography>
                 <TableContainer>
                     <Table>
                         <TableBody>
@@ -54,7 +56,6 @@ export default function ProductDetails(){
                         </TableBody>
                     </Table>
                 </TableContainer>
-
             </Grid>
         </Grid>
     )
