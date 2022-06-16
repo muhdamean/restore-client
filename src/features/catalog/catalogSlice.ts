91c3cf2fc5e0ad246f850c7193c810c09c9efebf
muhdamean <muhdamean@gmail.com>
2022-06-16 12:20:44 +0100

    ADD: Error Handling in Reducers

--- src/features/catalog/catalogSlice.ts
+++ src/features/catalog/catalogSlice.ts
@@ -7,22 +7,22 @@ const productsAdapter=createEntityAdapter<Product>();
 
 export const fetchProductsAsync=createAsyncThunk<Product[]>(
     'catalog/fetchProductsAsync',
-    async()=>{
+    async(_, thunkAPI)=>{
         try{
             return await agent.Catalog.list();
-        }catch(error){
-            console.log(error);
+        }catch(error:any){
+            return thunkAPI.rejectWithValue({error: error.data})
         }
     }
 )
 
 export const fetchProductAsync=createAsyncThunk<Product, number>(
     'catalog/fetchProductAsync',
-    async(productId)=>{
+    async(productId, thunkAPI)=>{
         try{
             return await agent.Catalog.details(productId);
-        }catch(error){
-            console.log(error);
+        }catch(error:any){
+            return thunkAPI.rejectWithValue({error:error.data})
         }
     }
 )
@@ -43,7 +43,8 @@ export const catalogSlice=createSlice({
             state.status='idle';
             state.productsLoaded=true;
         });
-        builder.addCase(fetchProductsAsync.rejected, (state)=>{
+        builder.addCase(fetchProductsAsync.rejected, (state,action)=>{
+            console.log(action.payload);
             state.status='idle';
         });
         builder.addCase(fetchProductAsync.pending, (state)=>{
@@ -53,7 +54,8 @@ export const catalogSlice=createSlice({
             productsAdapter.upsertOne(state, action.payload) 
             state.status='idle';
         });
-        builder.addCase(fetchProductAsync.rejected, (state)=>{
+        builder.addCase(fetchProductAsync.rejected, (state, action)=>{
+            console.log(action);
             state.status='idle';
         });
     })
