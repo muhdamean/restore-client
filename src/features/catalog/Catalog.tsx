535b4eac2a9f5e3aad5148998d20e95aff4b282f
muhdamean <muhdamean@gmail.com>
2022-06-16 11:35:35 +0100

    ADD: Entity Adapters Selector in catalogSlice

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from "react";
-import agent from "../../app/api/agent";
+import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Product } from "../../app/models/product"
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
 
 
 export default function Catalog(){
-    const [products,setProducts]=useState<Product[]>([]);
-    const [loading,setLoading]=useState(true);
+    const products=useAppSelector(productSelectors.selectAll);
+    const {productsLoaded,status}=useAppSelector(state=>state.catalog);
+    const dispatch=useAppDispatch();
 
   useEffect(()=>{
-      agent.Catalog.list()
-      .then(products=>setProducts(products))
-      .catch(error=>console.log(error))
-      .finally(()=>setLoading(false))
-  },[]) //adding empty array [] stands for !IsPostBack, should not re-render on whenever the content load. render once
+    if(!productsLoaded) dispatch(fetchProductsAsync());
+    //   agent.Catalog.list()
+    //   .then(products=>setProducts(products))
+    //   .catch(error=>console.log(error))
+    //   .finally(()=>setLoading(false))
+  },[productsLoaded,dispatch]) //adding empty array [] stands for !IsPostBack, should not re-render on whenever the content load. render once
 
-  if(loading) return <LoadingComponent message="Loading products..."></LoadingComponent>
+  if(status.includes('pending')) return <LoadingComponent message="Loading products..."></LoadingComponent>
 
     return(
         <>
--- src/features/catalog/catalogSlice.ts
+++ src/features/catalog/catalogSlice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
 import { Product } from "../../app/models/product";
+import { RootState } from "../../app/store/configureStore";
 
 const productsAdapter=createEntityAdapter<Product>();
 
@@ -36,3 +37,5 @@ export const catalogSlice=createSlice({
         });
     })
 })
+
+export const productSelectors=productsAdapter.getSelectors((state:RootState)=>state.catalog);
