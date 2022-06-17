1957f5068e878314ac419749be7a4fe18e52b140
muhdamean <muhdamean@gmail.com>
2022-06-17 08:14:49 +0100

    ADD: Sort to Client

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,8 +1,9 @@
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, Typography } from "@mui/material";
+import { Box, Checkbox, FormControlLabel, FormGroup, Grid, Pagination, Paper, Typography } from "@mui/material";
 import { useEffect } from "react";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
 import ProductSearch from "./ProductSearch";
 
@@ -14,7 +15,7 @@ const sortOptions=[
 
 export default function Catalog(){
     const products=useAppSelector(productSelectors.selectAll);
-    const {productsLoaded,status, filtersLoaded, brands, types}=useAppSelector(state=>state.catalog);
+    const {productsLoaded,status, filtersLoaded, brands, types, productParams}=useAppSelector(state=>state.catalog);
     const dispatch=useAppDispatch();
 
   useEffect(()=>{
@@ -34,13 +35,10 @@ export default function Catalog(){
                   <ProductSearch></ProductSearch>
                 </Paper>
                 <Paper sx={{mb:2, p:2}}>
-                <FormControl component="fieldset">
-                  <RadioGroup>
-                    {sortOptions.map(({value, label})=>(
-                        <FormControlLabel value={value} control={<Radio />} label={label} key={value} />
-                    ))}
-                  </RadioGroup>
-                </FormControl>
+                  <RadioButtonGroup 
+                    selectedValue={productParams.orderBy}
+                    options={sortOptions}
+                    onChange={(e)=>dispatch(setProductParams({orderBy:e.target.value}))}></RadioButtonGroup>
                 </Paper>
 
                 <Paper sx={{mb:2, p:2}}>
