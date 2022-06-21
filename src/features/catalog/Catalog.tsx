9e29e63533646b0cd813cd8680a4129cf0bc6056
muhdamean <muhdamean@gmail.com>
2022-06-21 13:47:12 +0100

    ADD: ProductCardSkeleton Skeleton Loading

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -17,7 +17,7 @@ const sortOptions=[
 
 export default function Catalog(){
     const products=useAppSelector(productSelectors.selectAll);
-    const {productsLoaded,status, filtersLoaded, brands, types, productParams, metaData}=useAppSelector(state=>state.catalog);
+    const {productsLoaded, filtersLoaded, brands, types, productParams, metaData}=useAppSelector(state=>state.catalog);
     const dispatch=useAppDispatch();
 
   useEffect(()=>{
@@ -28,7 +28,7 @@ export default function Catalog(){
     if(!filtersLoaded) dispatch(fetchFilters());
   },[dispatch, filtersLoaded])
 
-  if(status.includes('pending') || !metaData) return <LoadingComponent message="Loading products..."></LoadingComponent>
+  if(!filtersLoaded ) return <LoadingComponent message="Loading products..."></LoadingComponent>
 
     return(
         <Grid container columnSpacing={4}>
@@ -71,9 +71,11 @@ export default function Catalog(){
              </Grid>
              <Grid item xs={3}></Grid>
              <Grid item xs={9} sx={{mb:2}}>
+              {metaData &&
                 <AppPagination 
                   metaData={metaData}
                   onPageChange={(page: number)=> dispatch(setPageNumber({pageNumber:page}))}/>
+              }
              </Grid>
         </Grid>
         
