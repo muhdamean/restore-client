754b1341941c4a14930bb24813cb292286973c45
muhdamean <muhdamean@gmail.com>
2022-07-05 23:03:51 +0100

    CODE: Add react-hook-form validators

--- src/features/account/Login.tsx
+++ src/features/account/Login.tsx
@@ -13,10 +13,16 @@ import { LoadingButton } from '@mui/lab';
 
 
 export default function Login() {
-    const{register, handleSubmit, formState: {isSubmitting}}=useForm()
+    const{register, handleSubmit, formState: {isSubmitting, errors, isValid}}=useForm({
+        mode:'onTouched'
+    })
 
     async function submitForm(data:FieldValues){
-       await  agent.Account.login(data);
+       try{
+            await  agent.Account.login(data);
+       }catch(error){
+        console.log(error);
+       }
     }
 
     // const [values, setValues]=useState({
@@ -49,17 +55,23 @@ export default function Login() {
               fullWidth
               label="Username"
               autoFocus
-              {...register('username')}
+              {...register('username', {required:'Username is required'})}
+              error={!!errors.username}
+              helperText={errors?.username?.message}
             />
             <TextField
               margin="normal"
               fullWidth
               label="Password"
               type="password"
-              {...register('password')}
+              {...register('password', {required:'Password is required'})}
+              error={!!errors.password}
+              helperText={errors?.password?.message}
+
             />
             
             <LoadingButton loading={isSubmitting}
+                disabled={!isValid}
               type="submit"
               fullWidth
               variant="contained"
