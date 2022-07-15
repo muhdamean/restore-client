a71b8f1486e084df070319863a7d039363d0c1a6
muhdamean <muhdamean@gmail.com>
2022-07-15 16:17:54 +0100

    CODE: Update the Review Component

--- src/features/basket/BasketPage.tsx
+++ src/features/basket/BasketPage.tsx
@@ -1,14 +1,11 @@
-import { Add, Delete, Remove } from "@mui/icons-material";
-import { LoadingButton } from "@mui/lab";
-import { Box, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { addBasketItemAsync, removeBasketItemAsync } from "./basketSlice";
+import { useAppSelector } from "../../app/store/configureStore";
 import BasketSummary from "./BasketSummary";
+import BasketTable from "./BasketTable";
 
 export default function Basketpage(){
-    const {basket,status}=useAppSelector(state=>state.basket);
-    const dispatch=useAppDispatch();
+    const {basket}=useAppSelector(state=>state.basket);
     // const [status,setStatus]=useState({
     //     loading:false,
     //     name:''
@@ -34,62 +31,7 @@ export default function Basketpage(){
     
     return(
         <>
-        <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Product</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="center">Quantity</TableCell>
-              <TableCell align="right">Subtotal</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {basket.items.map((item) => (
-              <TableRow
-                key={item.productId}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                    <Box display='flex' alignItems='center'>
-                        <img src={item.pictureUrl} alt={item.name} style={{height:50, marginRight:20}}></img>
-                        <span>{item.name}</span>
-                    </Box>
-                </TableCell>
-                <TableCell align="right">{item.price}</TableCell>
-                <TableCell align="center">
-                    <LoadingButton 
-                        loading={status==='pendingRemoveItem'+item.productId+'rem'} 
-                        onClick={()=>dispatch(removeBasketItemAsync({
-                          productId: item.productId, quantity:1, name:'rem'
-                          }))} 
-                          color='error'>
-                        <Remove></Remove>
-                    </LoadingButton>
-                    {item.quantity}
-                    <LoadingButton 
-                        loading={status==='pendingAddItem'+item.productId} 
-                        onClick={()=>dispatch(addBasketItemAsync({productId: item.productId}))} color='secondary'>
-                        <Add></Add>
-                    </LoadingButton>
-                </TableCell>
-                <TableCell align="right">${((item.price/100)* item.quantity).toFixed(2)}</TableCell>
-                <TableCell align="right">
-                    <LoadingButton 
-                        loading={status==='pendingRemoveItem'+item.productId+'del'} 
-                        onClick={()=>dispatch(removeBasketItemAsync({
-                          productId: item.productId, quantity: item.quantity, name:'del'
-                          }))} 
-                          color='error'>
-                        <Delete></Delete>
-                    </LoadingButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+        <BasketTable items={basket.items}></BasketTable>
       <Grid container>
         <Grid item xs={6}/>
         <Grid item xs={6}>
